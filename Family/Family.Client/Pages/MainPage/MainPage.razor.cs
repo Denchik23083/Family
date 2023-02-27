08b5b0c404972f6308f2f28eaf54f410c780d08b
Denchik23083 <deniskudravov228@gmail.com>
2023-02-27 17:30:18 +0200

    Some css refactoring

--- Family/Family.Client/Pages/MainPage/MainPage.razor.cs
+++ Family/Family.Client/Pages/MainPage/MainPage.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace Family.Client.Pages.MainPage
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Family.Client.Pages.MainPage
 {
     public partial class MainPage
     {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public void AddParent()
+        {
+            NavigationManager.NavigateTo("/parents/add");
+        }
+
+        public void AddChild()
+        {
+            NavigationManager.NavigateTo("/children/add");
+        }
     }
 }
