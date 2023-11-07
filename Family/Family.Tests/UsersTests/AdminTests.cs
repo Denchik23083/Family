8191fc4092be2af3668507168fd32bb93085efb0
Denchik23083 <deniskudravov228@gmail.com>
2023-11-07 18:29:22 +0200

    Admin and God tests is done

---
+++ Family/Family.Tests/UsersTests/AdminTests.cs
@@ -0,0 +1,86 @@
+﻿using Family.Db.Entities;
+using Family.Logic.UsersService.AdminService;
+using Family.Tests.Utilities;
+using Family.WebDb.UsersRepository.AdminRepository;
+using Family.WebDb.UsersRepository.UserRepository;
+using Moq;
+using Xunit;
+
+namespace Family.Tests.UsersTests
+{
+    public class AdminTests : AuthUser
+    {
+        private readonly Mock<IAdminRepository> _repository;
+        private readonly Mock<IUserRepository> _userRepository;
+
+        public AdminTests()
+        {
+            _repository = new Mock<IAdminRepository>();
+            _userRepository = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async Task GetAdmins()
+        {
+            var roleId = 2;
+
+            var admins = new List<User>
+            {
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Admin",
+                    LastName = "Full",
+                    Email = "admin@gmail.com",
+                    Password = "0000",
+                    BirthDay = DateTime.Now.AddYears(-30),
+                    GenderId = 1,
+                    RoleId = roleId
+                }
+            };
+
+            _repository.Setup(_ => _.GetAdminsAsync(roleId))
+                .ReturnsAsync(admins);
+
+            IAdminService service = new AdminService(_repository.Object, _userRepository.Object);
+
+            var result = await service.GetAdminsAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(admins.Count, result.Count());
+        }
+
+        [Fact]
+        public async Task RemoveUser()
+        {
+            var expectedId = 5;
+
+            var userToRemove = new User
+            {
+                Id = expectedId,
+                FirstName = "Denis",
+                LastName = "Kudryavov",
+                Email = "denis@gmail.com",
+                Password = "0000",
+                BirthDay = new DateTime(2003, 08, 23),
+                GenderId = 1,
+                RoleId = 4
+            };
+
+            _userRepository.Setup(_ => _.GetUserAsync(expectedId))
+                .ReturnsAsync(userToRemove);
+
+            _repository.Setup(_ => _.RemoveUserAsync(userToRemove));
+
+            IAdminService service = new AdminService(_repository.Object, _userRepository.Object);
+
+            await service.RemoveUserAsync(expectedId);
+
+            _userRepository.Verify(_ => _.GetUserAsync(expectedId),
+                Times.Once);
+
+            _repository.Verify(_ => _.RemoveUserAsync(userToRemove),
+                Times.Once);
+        }
+    }
+}
---
+++ Family/Family.Tests/UsersTests/GodTests.cs
@@ -0,0 +1,96 @@
+﻿using Family.Db.Entities;
+using Family.Logic.UsersService.GodService;
+using Family.Tests.Utilities;
+using Family.WebDb.UsersRepository.GodRepository;
+using Family.WebDb.UsersRepository.UserRepository;
+using Moq;
+using Xunit;
+
+namespace Family.Tests.UsersTests
+{
+    public class GodTests : AuthUser
+    {
+        private readonly Mock<IGodRepository> _repository;
+        private readonly Mock<IUserRepository> _userRepository;
+
+        public GodTests()
+        {
+            _repository = new Mock<IGodRepository>();
+            _userRepository = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async Task UserToAdmin()
+        {
+            var expectedRoleId = 2;
+
+            var expectedId = 5;
+
+            var userToAdmin = new User
+            {
+                Id = expectedId,
+                FirstName = "Denis",
+                LastName = "Kudryavov",
+                Email = "denis@gmail.com",
+                Password = "0000",
+                BirthDay = new DateTime(2003, 08, 23),
+                GenderId = 1,
+                RoleId = 5
+            };
+
+            _userRepository.Setup(_ => _.GetUserAsync(expectedId))
+                .ReturnsAsync(userToAdmin);
+
+            _repository.Setup(_ => _.UserToAdminAsync(userToAdmin));
+
+            IGodService service = new GodService(_repository.Object, _userRepository.Object);
+
+            await service.UserToAdminAsync(expectedId);
+
+            _userRepository.Verify(_ => _.GetUserAsync(expectedId),
+                Times.Once);
+
+            _repository.Verify(_ => _.UserToAdminAsync(userToAdmin),
+                Times.Once);
+
+            Assert.Equal(expectedRoleId, userToAdmin.RoleId);
+        }
+
+        [Fact]
+        public async Task AdminToUser()
+        {
+            var expectedRoleId = 5;
+
+            var expectedId = 2;
+
+            var adminToUser = new User
+            {
+                Id = expectedId,
+                FirstName = "Admin",
+                LastName = "Full",
+                Email = "admin@gmail.com",
+                Password = "0000",
+                BirthDay = DateTime.Now.AddYears(-30),
+                GenderId = 1,
+                RoleId = 2
+            };
+
+            _userRepository.Setup(_ => _.GetUserAsync(expectedId))
+                .ReturnsAsync(adminToUser);
+
+            _repository.Setup(_ => _.AdminToUserAsync(adminToUser));
+
+            IGodService service = new GodService(_repository.Object, _userRepository.Object);
+
+            await service.AdminToUserAsync(expectedId);
+
+            _userRepository.Verify(_ => _.GetUserAsync(expectedId),
+                Times.Once);
+
+            _repository.Verify(_ => _.AdminToUserAsync(adminToUser),
+                Times.Once);
+
+            Assert.Equal(expectedRoleId, adminToUser.RoleId);
+        }
+    }
+}
