ef934405a8ebea0e4063bb68dc2565f604e41b3f
Denchik23083 <deniskudravov228@gmail.com>
2023-12-03 15:27:22 +0200

    Bug

--- Family/Family.Users/Utilities/MappingProfile.cs
+++ Family/Family.Users/Utilities/MappingProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Family.Db.Entities;
+using Family.Web.Models.ChildModels;
 using Family.Web.Models.GenderModels;
+using Family.Web.Models.GenusModels;
+using Family.Web.Models.ParentModels;
 using Family.Web.Models.UserModels;
 
 namespace Family.Users.Utilities
@@ -14,6 +17,16 @@ namespace Family.Users.Utilities
 
             CreateMap<Gender, GenderReadModel>();
             CreateMap<GenderWriteModel, Gender>();
+
+            CreateMap<Genus, GenusReadNameModel>();
+            CreateMap<Genus, GenusReadModel>();
+            CreateMap<GenusWriteModel, Genus>();
+
+            CreateMap<Parent, ParentReadModel>();
+            CreateMap<ParentWriteModel, Parent>();
+
+            CreateMap<Child, ChildReadModel>();
+            CreateMap<ChildWriteModel, Child>();
         }
     }
 }
--- Family/Family.Web/Utilities/MappingProfile.cs
+++ Family/Family.Web/Utilities/MappingProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Family.Db.Entities;
 using Family.Web.Models.ChildModels;
+using Family.Web.Models.GenderModels;
 using Family.Web.Models.GenusModels;
 using Family.Web.Models.ParentModels;
+using Family.Web.Models.UserModels;
 
 namespace Family.Web.Utilities
 {
@@ -10,6 +12,12 @@ namespace Family.Web.Utilities
     {
         public MappingProfile()
         {
+            CreateMap<User, UserReadModel>();
+            CreateMap<UserWriteModel, User>();
+
+            CreateMap<Gender, GenderReadModel>();
+            CreateMap<GenderWriteModel, Gender>();
+
             CreateMap<Genus, GenusReadNameModel>();
             CreateMap<Genus, GenusReadModel>();
             CreateMap<GenusWriteModel, Genus>();
