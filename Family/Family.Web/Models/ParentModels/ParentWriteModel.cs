ccc559779bb0f262ee89a69e239ae08076f85f2e
Denchik23083 <deniskudravov228@gmail.com>
2023-12-26 22:08:44 +0200

    Create genus is done

--- Family/Family.Web/Models/ParentModels/ParentWriteModel.cs
+++ Family/Family.Web/Models/ParentModels/ParentWriteModel.cs
@@ -3,10 +3,16 @@ using Family.Web.Models.UserModels;
 
 namespace Family.Web.Models.ParentModels
 {
-    public class ParentWriteModel
+    public class ParentReadModel
     {
         public int Id { get; set; }
 
+        public UserReadModel? User { get; set; }
+
         public int UserId { get; set; }
+
+        public int? GenusId { get; set; }
+
+        public GenusReadModel? Genus { get; set; }
     }
 }
